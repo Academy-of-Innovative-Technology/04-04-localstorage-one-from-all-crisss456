2f26a80cdfc6eaf1981e5eeeb78298e59d9aab66
“cristopher” <curibe26@aoiths.org>
2026-03-11 13:54:27 -0400

    update

--- src/script/script.js
+++ src/script/script.js
@@ -1,11 +1,28 @@
 var Database = {
-  keyName: "Mutant Database",
+  keyName: "mutantDatabase",
   data: []
 };
 
 function loadDataSource() {
 
+  // get data from localStorage
+  var storedData = localStorage.getItem(Database.keyName);
 
+  // convert JSON string into JavaScript object
+  var parsedData = JSON.parse(storedData);
+
+  // save data into Database object
+  Database.data = parsedData.response;
+
+  // fill dropdown with mutant aliases
+  var select = document.querySelector("#select-mutant");
+
+  for (var i = 0; i < Database.data.length; i++) {
+    var option = document.createElement("option");
+    option.text = Database.data[i].name.alias;
+    option.value = i;
+    select.appendChild(option);
+  }
 }
 
 
